fc159a188741a2c3c2f4c984c42dc1efe7fd3fd0
Jorge Gregorio Donaire Pereyra <jorgedonaire93@gmail.com>
2025-06-10 16:50:16 -0300

    iniciado punto 1

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("**** INICIO ****");
+Console.WriteLine(" ");
+string rutaDirectorio = @"c:\prueba\";
+if (Directory.Exists(rutaDirectorio))
+{
+    Console.WriteLine("**** Carpetas en el directorio ****");
+    var rutaDirectorioConvertida = new DirectoryInfo(rutaDirectorio);
+    DirectoryInfo[] arregloDeDirectorios = rutaDirectorioConvertida.GetDirectories();
+
+    foreach (DirectoryInfo carpeta in arregloDeDirectorios)
+    {
+        Console.WriteLine(carpeta.Name);
+    }
+    Console.WriteLine(" ");
+    Console.WriteLine("**** Archivos en el directorio ****");
+    foreach (var archivo in rutaDirectorioConvertida.GetFiles())
+    {
+        Console.WriteLine(archivo.Name + "                      " + archivo.Length + "kb");
+    }
+
+    string rutaArchivoCsv = $"{rutaDirectorio}reporte_archivos.csv";
+    if (!File.Exists(rutaArchivoCsv))
+    {
+        File.Create(rutaArchivoCsv);
+        List<string> lineasCsv = new List<string>();
+        lineasCsv.Add("Nombre del archivo, Tamaño (KB), Fecha de ultima modificacion"); //inicializo la cabecera
+        foreach (var archivo in rutaDirectorioConvertida.GetFiles())
+        {
+            string linea = $"{archivo.Name},{archivo.Length},{archivo.LastWriteTime}"; //concateno los datos del archivo
+            lineasCsv.Add(linea);
+        }
+        // File.WriteAllLines(rutaArchivoCsv, lineasCsv); //otra forma 
+    }
+}
+else
+{
+    Console.WriteLine("El directorio ingresado no existe. Ingrese nuevamente");
+}
